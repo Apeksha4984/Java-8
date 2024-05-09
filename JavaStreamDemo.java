90ed7f63daf46032c28cefa3ddc52845402a0385
Apeksha Rane <121625467+Apeksha4984@users.noreply.github.com>
2024-05-09 18:07:21 +0530

    Update JavaStreamDemo.java

--- JavaStreamDemo.java
+++ JavaStreamDemo.java
@@ -26,14 +26,14 @@ public class JavaStreamDemo {
 
 	public static void main(String[] args) {
 		List<A>studentList=new ArrayList<A>();
-		studentList.add(new A(1,"ashish","RWI140","Rewa"));
-		studentList.add(new A(2,"Sneha","RWI131","Anuppur"));
-		studentList.add(new A(3,"Ankit","RWI132","Durg"));
-		studentList.add(new A(4,"Sakshi","RWI127","Bhilai"));
-		studentList.add(new A(5,"siddharth","RWI151","Durg"));
+		studentList.add(new A(1,"apeksha","RWI501","Betul"));
+		studentList.add(new A(2,"Anjali","RWI502","Indore"));
+		studentList.add(new A(3,"Aarav","RWI503","Delhi"));
+		studentList.add(new A(4,"kavita","RWI504","Bhopal"));
+		studentList.add(new A(5,"arpita","RWI506","Gurugram"));
 		
 		List<Integer>statelist=studentList.stream()
-                           .filter(p->p.sid>3)//Filering
+                           .filter(p->p.sid>3)//Filtering
                            .map(m->m.sid).//fetching
                            collect(Collectors.toList());
 		System.out.println(statelist);
